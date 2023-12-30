694e5e31ffd3fe06873de098e7507f4ccb917500
Murat <muratbakhriev@gmail.com>
2023-12-30 12:45:06 +0500

    drodown select

--- src/assets/js/functions/dropdown.js
+++ src/assets/js/functions/dropdown.js
@@ -1,6 +1,16 @@
 export const dropdownToggle = () => {
 	const dropdowns = document.querySelectorAll('.dropdown')
 	const toggles = document.querySelectorAll('.dropdown__toggle')
+	const items = document.querySelectorAll('.dropdown__item')
+	const dropdownInput = document.querySelector('.dropdown__input')
+
+	items?.forEach(item => {
+		item.addEventListener('click', e => {
+			dropdownInput.value = item.textContent
+			e.target.closest('.dropdown').classList.remove('dropdown_active')
+		})
+	})
+
 	toggles?.forEach(toggle => {
 		toggle.addEventListener('click', () => {
 			const dropdown = toggle.closest('.dropdown')
